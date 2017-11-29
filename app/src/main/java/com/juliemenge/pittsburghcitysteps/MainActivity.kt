f4885feddc39c7b5946ca3a2dcf3866e92fb50d0
Julie Menge <juliemenge@gmail.com>
2017-11-28 21:14:11 -0500

    added recyclerview

--- app/src/main/java/com/juliemenge/pittsburghcitysteps/MainActivity.kt
+++ app/src/main/java/com/juliemenge/pittsburghcitysteps/MainActivity.kt
@@ -1,58 +1,83 @@
 package com.juliemenge.pittsburghcitysteps
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.widget.ListView
+import android.widget.LinearLayout
+import com.github.nitrico.lastadapter.LastAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
 import org.jetbrains.anko.coroutines.experimental.bg
 import org.json.JSONObject
-import java.util.ArrayList
+import java.util.*
+import kotlin.collections.ArrayList
+import android.R.array
+import java.util.Arrays.asList
+
+
 
 class MainActivity : AppCompatActivity() {
-
-    //internal lateinit var stepList: Array<Step?>
-    //lateinit var jsonData: String
-    //val stepList = arrayOfNulls<Step>(863) //create an array of steps with no data
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //getting recyclerview from xml
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        //adding a layoutmanager
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+
+
         val rawNeighborhoodChoice = "Upper Lawrenceville" //TODO: will probably get this from a spinner, adding leading and trailing apostrophes below
         val neighborhoodChoice = "'" + rawNeighborhoodChoice + "'"
 
-        async(UI){
+        async(UI) {
             val network = NetworkApi() //pass in user selected neighborhood
-            val data = bg{network.getDataFromServer(neighborhoodChoice)}
+            val data = bg { network.getDataFromServer(neighborhoodChoice) }
             val useableData = data.await()
 
             val allthedata = JSONObject(useableData) //get one giant blob of json data
             val result = allthedata.getJSONObject("result") //create a json blob of just the result data
             val records = result.getJSONArray("records") //get an array of the records
 
-            var stepList = arrayOfNulls<Step>(records.length()) //create an array of steps with no data
+            val arraySize = records.length()
+
+            //val stepList: Array<Step?> = arrayOfNulls<Step>(records.length()) //create an array of steps with no data
+            //val stepList: ArrayList<Step?> = ArrayList<Step?>(records.length())
+            val stepList = ArrayList<Step>(arraySize)
 
             //iterate through json data and create array of steps objects
 
-            for (i in stepList.indices) {
+            //for (i in stepList.indices) {
+            for (i in 0..arraySize-1) {
                 val jsonStep = records.getJSONObject(i) //do stuff with the first step
                 val step = Step() //create a new step object
                 step.neighborhood = jsonStep.getString("neighborhood")
                 step.name = jsonStep.getString("name")
                 step.material = jsonStep.getString("material")
                 step.length = jsonStep.getInt("length")
-                stepList[i] = step
+                //stepList[i] = step
+                stepList.add(step)
                 Log.d(javaClass.simpleName, "Number: " + i + ", Neighborhood: " + step.neighborhood + ", Name: " + step.name + ", Material: " + step.material + ", Length: " + step.length)
             }
 
-            //TODO: create a recycler view to display steplist
-            Log.d(javaClass.simpleName, "HI THERE Neighborhood: " + stepList[0]!!.neighborhood + ", Name: " + stepList[0]!!.name + ", Material: " + stepList[0]!!.material + ", Length: " + stepList[0]!!.length)
-            //textView.setText("Neighborhood: " + step.neighborhood + ", Name: "+ step.name + ", Material: "  + step.material + ", Length: " + step.length)
-        }
+            //val newStepList = ArrayList(Arrays.asList(stepList))
 
+            //val newStepList = stepList.toCollection(ArrayList())
+
+            val adapter = RecyclerAdapter(stepList)
+            recyclerView.adapter = adapter
+
+                //TODO: create a recycler view to display steplist
+                Log.d(javaClass.simpleName, "HI THERE Neighborhood: " + stepList[0]!!.neighborhood + ", Name: " + stepList[0]!!.name + ", Material: " + stepList[0]!!.material + ", Length: " + stepList[0]!!.length)
+                //textView.setText("Neighborhood: " + step.neighborhood + ", Name: "+ step.name + ", Material: "  + step.material + ", Length: " + step.length)
+            }
+
+        }
     }
-}
+
---
+++ app/src/main/java/com/juliemenge/pittsburghcitysteps/RecyclerAdapter.kt
@@ -0,0 +1,36 @@
+package com.juliemenge.pittsburghcitysteps
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class RecyclerAdapter(val stepList: ArrayList<Step>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    //this method is returning the view for each item in the list
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_row, parent, false)
+        return ViewHolder(v)
+    }
+
+    //this method is binding the data on the list
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        holder.bindItems(stepList[position])
+    }
+
+    //this method is giving the size of the list
+    override fun getItemCount(): Int {
+        return stepList.size
+    }
+
+    //the class is hodling the list view
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindItems(step: Step) {
+            val textViewName = itemView.findViewById<TextView>(R.id.stepName)
+            val textViewNeighborhood  = itemView.findViewById<TextView>(R.id.stepNeighborhood)
+            textViewName.text = step.name
+            textViewNeighborhood.text = step.neighborhood
+        }
+    }
+}
